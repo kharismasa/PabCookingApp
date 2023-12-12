36b190bc2c2599f87037f4aefbf2cc1ebf050ef5
Kharisma Suci A <21523244@students.uii.ac.id>
2023-12-12 07:47:40 +0700

    Rekomendasi Fix
    Fitur rekomendasi Fixed

--- Bismillah/app/src/main/java/com/example/bismillah/ui/fragments/home/HomeFragment.kt
+++ Bismillah/app/src/main/java/com/example/bismillah/ui/fragments/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.bismillah.ui.fragments.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,6 +14,11 @@ import com.example.bismillah.databinding.FragmentHomeBinding
 import com.example.bismillah.util.Constants.Companion.API_KEY
 import com.example.bismillah.util.NetworkResult
 import com.example.bismillah.viewmodels.MainViewModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -21,6 +27,7 @@ class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
     private val recomendationAdapter by lazy { RecomendationAdapter() }
     private lateinit var mainViewModel: MainViewModel
+    private lateinit var databaseReference: DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,14 +39,75 @@ class HomeFragment : Fragment() {
         binding.mainViewModel = mainViewModel
 
 
+        // Inisialisasi referensi database Firebase
+        databaseReference = FirebaseDatabase.getInstance().getReference("users").child("Kharisma")
+
+        // Dapatkan preferensi pengguna dari Firebase
+        getPreferencesFromFirebase()
+
         setupRecyclerView()
-        requestApiData()
 
         return binding.root
     }
 
-    private fun requestApiData() {
-        mainViewModel.getRecipes(applyQueries())
+    private fun getPreferencesFromFirebase() {
+        databaseReference.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    // Pengguna ditemukan di Firebase
+                    val alergiPreferences = snapshot.child("preferensi alergi")
+                    val dislikePreferences = snapshot.child("preferensi dislike")
+
+                    // Bangun excludeIngredientsList berdasarkan preferensi
+                    val excludeIngredientsList = buildExcludeIngredientsList(alergiPreferences, dislikePreferences)
+
+                    // Gunakan excludeIngredientsList untuk mendapatkan resep
+                    requestApiData(excludeIngredientsList)
+                } else {
+                    // Pengguna tidak ditemukan di Firebase
+                    // requestApiData tanpa excludeIngredientsList (kosong)
+                    requestApiData(emptyList())
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Handle error
+                Log.e("HomeFragment", "Error fetching user preferences: ${error.message}")
+            }
+        })
+    }
+
+    private fun buildExcludeIngredientsList(
+        alergiPreferences: DataSnapshot,
+        dislikePreferences: DataSnapshot
+    ): List<String> {
+        val excludeIngredientsList = mutableListOf<String>()
+
+        // Tambahkan bahan dari preferensi alergi
+        alergiPreferences.children.forEach { alergi ->
+            if (alergi.value as Boolean) {
+                excludeIngredientsList.add(alergi.key!!)
+            }
+        }
+
+        // Tambahkan bahan dari preferensi dislike
+        dislikePreferences.children.forEach { dislike ->
+            if (dislike.value as Boolean) {
+                excludeIngredientsList.add(dislike.key!!)
+            }
+        }
+
+        return excludeIngredientsList
+    }
+
+    private fun setupRecyclerView() {
+        binding.rekomendasiRecyclerView.adapter = recomendationAdapter
+        binding.rekomendasiRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        showShimmerEffect()
+    }
+
+    private fun requestApiData(excludeIngredientsList: List<String>) {
+        mainViewModel.getRecipes(applyQueries(excludeIngredientsList))
         mainViewModel.recipesResponse.observe(viewLifecycleOwner) { response ->
             when (response) {
                 is NetworkResult.Success -> {
@@ -63,23 +131,22 @@ class HomeFragment : Fragment() {
         }
     }
 
-    private fun applyQueries(): HashMap<String, String> {
+    private fun applyQueries(excludeIngredientsList: List<String>): HashMap<String, String> {
         val queries: HashMap<String, String> = HashMap()
 
         queries["number"] = "50"
         queries["apiKey"] = API_KEY
-        queries["type"] = "snack"
-        queries["diet"] = "vegan"
+//        queries["type"] = "snack"
+//        queries["diet"] = "vegan"
         queries["addRecipeInformation"] = "true"
         queries["fillIngredients"] = "true"
 
-        return queries
-    }
+        // Tambahkan query untuk excludeIngredientsList
+        if (excludeIngredientsList.isNotEmpty()) {
+            queries["excludeIngredients"] = excludeIngredientsList.joinToString(",")
+        }
 
-    private fun setupRecyclerView() {
-        binding.rekomendasiRecyclerView.adapter = recomendationAdapter
-        binding.rekomendasiRecyclerView.layoutManager = LinearLayoutManager(requireContext())
-        showShimmerEffect()
+        return queries
     }
 
     private fun showShimmerEffect() {
